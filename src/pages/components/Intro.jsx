96a02e98f0166018f21f04185df6e192609b794e
JacobDFrank <frankjacob42@gmail.com>
2017-12-25 04:01:23 -0500

    finishing linting idea

--- src/pages/components/Intro.jsx
+++ src/pages/components/Intro.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 class Intro extends React.Component {
 
   render() {
-    return (<section className="projects">
-      <h2>
-        <b>Hey hey</b>
-        —
-        <b>I'm Jacob Frank, a creative developer, designer, and 3rd year student at RIT</b>
-      </h2>
-    </section>
+    return (
+      <section className="projects">
+        <h2>
+          <b>Hey hey</b>
+          —
+          <b>I&apos;m Jacob Frank, a creative developer, designer, and 3rd year student at RIT</b>
+        </h2>
+      </section>
     );
   }
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,28 +5,31 @@ import Footer from './components/Footer'
 import Intro from './components/Intro'
 import Header from './components/Header'
 import Bubble from './components/Bubble'
-import AboutMe from './components/AboutMe'
+// import AboutMe from './components/AboutMe'
 
-const IndexPage = ({data}) => (<div className="body home-colors">
-  <div id="app" className="wrapper">
-    <Header/>
-    <Intro/>
-    <section className="projects grid">
-      {
-        data.allMarkdownRemark.edges.map(post => (<div className="project grid__col grid__col--1-of-3" key={post.node.id}>
-          <span className="meta-data code">{post.node.frontmatter.tags}</span>
-          <Link to={post.node.frontmatter.path} className="faux-link">
-            <span data-volume={post.node.frontmatter.volume} data-homeImage={post.node.frontmatter.homeImage}>{post.node.frontmatter.description}</span>
-          </Link>
-        </div>))
-      }
-    </section>
-    <Footer/>
-  </div>
-  <Bubble/>
-</div>)
+const IndexPage = ({ data }) => (
+  <div className="body home-colors">
+    <div id="app" className="wrapper">
+      <Header />
+      <Intro />
+      <section className="projects grid">
+        {
+          data.allMarkdownRemark.edges.map(post => (
+            <div className="project grid__col grid__col--1-of-3" key={post.node.id}>
+              <span className="meta-data code">{post.node.frontmatter.tags}</span>
+              <Link to={post.node.frontmatter.path} href={post.node.frontmatter.path} className="faux-link">
+                <span data-volume={post.node.frontmatter.volume} data-homeImage={post.node.frontmatter.homeImage}>{post.node.frontmatter.description}</span>
+              </Link>
+            </div>
+          ))
+        }
+      </section>
+      <Footer />
+    </div>
+    <Bubble />
+  </div>)
 
-export const pageQuery = graphql `
+export const pageQuery = graphql`
   query IndexQuery { allMarkdownRemark(
     limit: 10,
     sort: { fields:[ frontmatter___date], order: ASC }
